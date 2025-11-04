9314f99015de0c2b775b76ea4b1fb2393a81ba21
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-04 13:47:47 +0100

    feat: funzione dei saluti completata

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = "Mario";
 
 
 // Dichiara la funzione qui.
+function saluto(names) {
+    console.log("Ciao " + names);
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+saluto(name);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
