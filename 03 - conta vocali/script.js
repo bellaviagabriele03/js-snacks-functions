efad38e694d97d0f79f6acab9fdd10bd389b7930
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-04 13:43:41 +0100

    feat: completata funzione che stampa il numero delle vocali e le vocali contenute in una parola

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocali(parole) {
+    let lettere = [];
+    for (let i = 0; i < parole.length; i++) {
+        let curIndex = parole[i];
+        if (curIndex === "a" || curIndex === "e" || curIndex === "i" || curIndex === "o" || curIndex === "u") {
+            lettere.push(curIndex)
+        }
+    }
+    return lettere.length, lettere;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
