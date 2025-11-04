b6e184f2f3320c7b70f6396860623de705872253
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-04 13:29:29 +0100

    Feat: completato funzione che prende i nomi con una lettera specifica iniziale

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,9 +4,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function startLetter(name, numIndex, letters) {
+    let newNames = [];    
+    for (let i = 0; i < name.length; i++) {
+        let curName = name[i];
+        
+        let firstIndex = curName[numIndex]
+        if (firstIndex === letters) {
+            newNames.push(curName);
+        }        
+        
+    }
+    return newNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+let result = startLetter(names, 0, "A");
+console.log(result);
 
 
 
