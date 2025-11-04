045bbaacc95836ee25cd12df32a256cb9e7bf1da
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-04 13:19:11 +0100

    Feat: function per le iniziali dei nomi completato

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,9 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali(nomi, numberLetter) {
+    let lettera = [];
+    for(let i = 0; i < nomi.length; i++) {
+        let nomeCor = nomi[i];
+        let firstIndex = nomeCor[numberLetter]
+        lettera.push(firstIndex);
+        
+    }
+    return lettera;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(iniziali(names, 0));
 
 
 
